3d1377f119b90d89c8a45abcd08d4e2ec624f390
ulisescorrales <ulisescorralesallen@gmail.com>
2026-06-15 00:20:39 -0300

    inicializando submatrices, falta comunicación de bordes

--- tp8/calor_paralelo.c
+++ tp8/calor_paralelo.c
@@ -1,4 +1,6 @@
-//gcc -o calor_serie calor_serie.c
+//mpicc -o calor_paralelo calor_paralelo.c -lm
+#include <unistd.h> 
+#include <math.h>
 #include </usr/lib/x86_64-linux-gnu/openmpi/include/mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,6 +10,28 @@
 #define Cx 0.1f
 #define Cy 0.1f
 
+struct Multiplicacion{
+	int numero1;
+	int numero2;
+};
+struct Multiplicacion getMultiplicacionBalanceada(int n){
+	//Consigue i*j=n más balanceado para armar la matriz
+	int raizEntera=(int)sqrt((double)n);
+	int divisor=raizEntera;
+	//Conseguir el divisor más grande menor a la raíz cuadrada
+	while(divisor>0 && n%divisor!=0){
+		divisor--;
+	}
+	//Conseguido el dividor, calcular el dividendo y retornar el objeto Multiplicacion
+	int dividendo=n/divisor;
+
+	struct Multiplicacion retornar={
+		dividendo,
+		divisor
+	};
+	return retornar;
+}
+
 double sampleTime() {
 	struct timespec tv;
 	clock_gettime(CLOCK_MONOTONIC,&tv);
@@ -15,24 +39,48 @@ double sampleTime() {
 }
 
 int main(int argc, char *argv[]) {
+	MPI_Init(&argc,&argv);
 	register int p, i, j; //Variables para iteración de bucles
 	if(argc != 3) {
 		printf("Parámetros: Tlado pasos\n");
 		exit(1);
 	}
 	int Tlado = atoi(argv[1]); //N --> Tamaño de la matriz cuadrada
+	
+	int cantProcesos;
+	MPI_Comm_size(MPI_COMM_WORLD,&cantProcesos);
+	//Por ahora se hace con una matriz con Tlado divisible por la cantidad de procesos
+	if(Tlado%cantProcesos!=0){
+		printf("No es divisible lado por cantidad de procesos %d\n",cantProcesos);
+		exit(1);
+	}
+
+	struct Multiplicacion multiplos=getMultiplicacionBalanceada(cantProcesos);
+
+
 	int pasos = atoi(argv[2]); //M --> Cantidad de pasos
 	if (Tlado <= 0 || pasos < 0) {
 		printf("ERROR: valores incorrectos en parámetros\n");
 		exit(1);
 	}
+	//TODO: se considera para matriz cuadrada y lado divisible por lado de grilla
+	int TladoProc=Tlado/multiplos.numero1;
 
-	int columnas = Tlado;
-	int filas = Tlado;
+	//crear grilla cuadrada
+	int dims[]={multiplos.numero1,multiplos.numero2};
+	int periods[]={0,0};
+	MPI_Comm cart;
+	MPI_Cart_create(MPI_COMM_WORLD,2,dims,periods,0,&cart);
+
+	int columnas = TladoProc;
+	int filas = TladoProc;
+	int columnasT=Tlado;
+	int filasT=Tlado;
 	float *fila_arriba = malloc(sizeof(float)*columnas);
 	float *fila_abajo = malloc(sizeof(float)*columnas);
 	float *columna_izquierda = malloc(sizeof(float)*filas);
 	float *columna_derecha = malloc(sizeof(float)*filas);
+	//Simulan los cero grados del exterior
 	bzero(fila_arriba, sizeof(float)*columnas);
 	bzero(fila_abajo, sizeof(float)*columnas);
 	bzero(columna_izquierda, sizeof(float)*filas);
@@ -43,12 +91,13 @@ int main(int argc, char *argv[]) {
 	float **aux; //Puntero auxiliar para intercambio de punteros
 
 	//Reserva de espacio para la matriz Actual
-	matrizActual = (float **)malloc(sizeof(float *) * Tlado);
-	*matrizActual = (float *)malloc(sizeof(float) * Tlado * Tlado);
+	matrizActual = (float **)malloc(sizeof(float *) * TladoProc);
+	//en matrizActual[0] se apunta el primer elemento de la matri completa
+	*matrizActual = (float *)malloc(sizeof(float) * TladoProc * TladoProc);
 
 	//Reserva de espacio para la matriz Siguiente
-	matrizSiguiente  = (float **)malloc(sizeof(float *) * Tlado);
-	*matrizSiguiente = (float *)malloc(sizeof(float) * Tlado * Tlado);
+	matrizSiguiente  = (float **)malloc(sizeof(float *) * TladoProc);
+	*matrizSiguiente = (float *)malloc(sizeof(float) * TladoProc * TladoProc);
 
 	if (matrizActual == NULL || matrizSiguiente == NULL){
 		printf("ERROR: No se pudo reservar memoria\n");
@@ -56,15 +105,34 @@ int main(int argc, char *argv[]) {
 	}
 
 	//Se calculan las direcciones de las filas
-	for(i = 1; i < Tlado; i++){
-		matrizActual[i] = *matrizActual + Tlado * i;
-		matrizSiguiente[i] = *matrizSiguiente + Tlado * i;
+	for(i = 1; i < TladoProc; i++){
+		//Para i=0 ya se hizo antes
+		//matrizActual[i] tiene puntero a la fila
+		matrizActual[i] = *matrizActual + TladoProc * i;
+		matrizSiguiente[i] = *matrizSiguiente + TladoProc * i;
 	}
+	//CALCULAR POSICION EN LA GRILLA
+	int rank;
+	MPI_Comm_rank(cart,&rank);
+	int coords[2];
+	MPI_Cart_coords(cart,rank,2,coords);
+	int filaP=coords[0];
+	int columnaP=coords[1];
 
 	//Inicialización de la matriz Actual
-	for (i = 0; i < Tlado; i++)
-		for (j = 0; j < Tlado; j++)
-			matrizActual[i][j] = (float)(i+1) * (Tlado + i) * (j+1) * (Tlado + j);
+	for (i = 0; i < TladoProc; i++)
+		for (j = 0; j < TladoProc; j++)
+			if(i>=filaP*TladoProc && i<((filaP+1)*TladoProc)
+					&& j>=columnaP*TladoProc && j<((columnaP+1)*TladoProc)
+				)
+			matrizActual[i][j] = (float)(i+1) * (TladoProc + i) * (j+1) * (TladoProc + j);
+	printf("Inicializado\n");
+	//INFORMAR Y RECIBIR BORDES
+	//
+	//
+
+	/* printf("proceso: %d, fila: %d, col: %d\n",rank,coords[0],coords[1]); */
+	/* exit(0); */
 
 	//Ejecución de los pasos de simulación
 	double time_spent = sampleTime();
@@ -72,9 +140,11 @@ int main(int argc, char *argv[]) {
 	float e_arriba, e_abajo, e_izq, e_der, yo;
 	for (p = 0; p < pasos; p++) {
 
+		printf("TladoProc: %d,filas: %d, columnas: %d\n",TladoProc,filas,columnas);
 		//Se procesa el interior
 		for (i = 1; i < filas-1; i++)
 			for (j = 1; j < columnas-1; j++) {
+				//Comprobar que esté dentro de nuestra submatriz
 				yo = matrizActual[i][j];
 				e_arriba = matrizActual[i-1][j];
 				e_abajo = matrizActual[i+1][j];
@@ -82,7 +152,7 @@ int main(int argc, char *argv[]) {
 				e_der = matrizActual[i][j+1];
 				matrizSiguiente[i][j] = yo+Cx*(e_abajo+e_arriba-2*yo)+Cy*(e_der+e_izq-2*yo);
 			}
-
+		exit(0);
 		//Se procesa fila superior
 		i = 0;
 		for (j = 1; j < columnas-1; j++) {
@@ -190,6 +260,7 @@ int main(int argc, char *argv[]) {
 	}
 	fclose(f);
 
+	MPI_Finalize();
 	return 0;
 }
 
